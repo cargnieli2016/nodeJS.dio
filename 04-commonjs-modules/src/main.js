33ba8cbd9d3a52d82eed06de6b24b81824ee5a43
Cargnieli2016 <cargnieli2016@gmail.com>
2025-07-02 07:15:04 -0300

    exports consts

--- 04-commonjs-modules/src/main.js
+++ 04-commonjs-modules/src/main.js
@@ -1,12 +1,19 @@
 const p = require("./service/products")
+const config = require("./service/config")
 
 async function main() {
-    console.log("\n")
-    console.log("             == Carrinho de Compras ==        ");
-    p.getFullNme("480", "Teclado 101 teclas");
-    p.getFullNme("481", "Mouse");
-    p.getFullNme("482", "MousePad");
-    console.log("\n")
+    // console.log("\n")
+    // console.log("             == Carrinho de Compras ==        ");
+    // p.getFullNme("480", "Teclado 101 teclas");
+    // p.getFullNme("481", "Mouse");
+    // p.getFullNme("482", "MousePad");
+    // console.log("\n")
+    // p.getProductLabel("mousePad")
+    // console.log("\n");
+    console.log("\n");
+    console.log("Está em produção?: " + config.production);
+    console.log("Versão do produto: " + config.version);
+    console.log("\n");
     
 }
 
--- 04-commonjs-modules/src/service/products.js
+++ 04-commonjs-modules/src/service/products.js
@@ -5,6 +5,12 @@ async function getFullNme(codeID, produtctName) {
   console.log("product: -> " + "CodeId: " + codeID + "    " + "ProductName: -> " + produtctName);
 }
 
+async function getProductLabel(productName) {
+  console.log("Product " + productName);
+  
+}
+
 module.exports = {
   getFullNme,
+  getProductLabel,
 };
