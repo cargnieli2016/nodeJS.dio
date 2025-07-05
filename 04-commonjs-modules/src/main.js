62a841216de58127c49d930d19e1fb94034a7395
Cargnieli2016 <cargnieli2016@gmail.com>
2025-07-05 05:01:29 -0300

    destructuring

--- 04-commonjs-modules/src/main.js
+++ 04-commonjs-modules/src/main.js
@@ -1,21 +1,27 @@
-const product = require("./service/products");
+const { getFullNme, productType } = require("./service/products");
+const products = require("./service/products");
+
 const config = require("./service/config");
 const database = require("./service/database");
 
 async function main() {
-    // console.log("\n")
-    // console.log("             == Carrinho de Compras ==        ");
-    // p.getFullNme("480", "Teclado 101 teclas");
-    // p.getFullNme("481", "Mouse");
-    // p.getFullNme("482", "MousePad");
-    // console.log("\n")
-    // p.getProductLabel("mousePad")
-    // console.log("\n");
-    console.log("\n");
-    console.log("Está em produção?: " + config.production);
-    console.log("Versão do produto: " + config.version);
-    console.log("\n");
-    
+  console.log("\n");
+  console.log("             == Carrinho de Compras ==        ");
+
+  getFullNme("1", "teclado");
+  products.getFullNme("1", "teclado");
+  //   product.getFullNme("480", "Teclado 101 teclas");
+  //   product.getFullNme("481", "Mouse");
+  //   product.getFullNme("482", "MousePad");
+  //   console.log("\n");
+  //   product.getProductLabel("mousePad");
+  console.log("\n");
+  console.log("Está em produção?: " + config.production);
+  console.log("Versão do produto: " + config.version);
+  console.log("\n");
+
+  database.connectToDataBase("my-data");
+  console.log("\n");
 }
 
 main();
